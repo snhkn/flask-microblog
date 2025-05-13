4a60e57ebf03ddf321b432683437cc15d8eaa97c
snhkn <senihaakin@gmail.com>
2025-05-13 22:20:22 +0300

    Add pagination to blog posts

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    POSTS_PER_PAGE = 3
