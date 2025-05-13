cd681fbd1ef7a96f1e0b0a1d9c09f51114c13e4a
snhkn <senihaakin@gmail.com>
2025-05-13 18:30:51 +0300

    Add following followed and get posts of followed users

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
