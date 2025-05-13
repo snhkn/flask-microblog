fc3658e083ebf8761e8908c4b9080e2b10fa382c
snhkn <senihaakin@gmail.com>
2025-05-13 15:40:34 +0300

    Add database, user and post models

--- microblog.py
+++ microblog.py
@@ -1,4 +1,8 @@
-from app import app
-from dotenv import load_dotenv
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
 
-load_dotenv()
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
